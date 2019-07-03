7bbf378b3cda505287abd6fc4e9d3fe8032fa407
Steeven <wilson972906@gmail.com>
2019-07-03 17:45:19 -0500

    nodemailer y socket.io

--- server/index.js
+++ server/index.js
@@ -20,4 +20,93 @@ fs.readFile('./credentials.json', (err, content)=>{
     autorize(JSON.parse(content), listLabels); //método para leer las etiquetas de gmail
 });
 
-//Crear un cliente oauth2 con las credenciales json y 
+/**
+ * Crear un cliente oauth2 con las credenciales json y luego ejecute las funciones de callback
+ * @param {Object} credentials Las credenciales de autorización del cliente
+ * @param {function} callback Las devoluciones de llamada para el cliente autorizado.
+ */
+function autorize(credentials, callback){
+    const {client_secret, client_id, redirect_uris} = credentials.installed;
+    const oAuth2Client = new google.auth.OAuth2(
+        client_id, client_secret, redirect_uris[0]);
+    
+    //Comprobar si se tiene almacenado previamente un token
+    fs.readFile(TOKEN_PATH, (err, token) => {
+        if(err) return getNewToken(oAuth2Client, callback);
+        oAuth2Client.setCredentials(JSON.parse(token));
+        callback(oAuth2Client);
+    });
+}
+/**
+ * Obtener y almacenar un nuevo token despues de solicitar la autenticación de usuario, y luego
+ * ejecuta la devolución de llamada dada con el cliente oAuth2 autorizado
+ * @param {google.auth.OAuth2} oAuth2Client El cliente OAuth2 para el que se obtiene el token.
+ * @param {getEventsCallback} callback La devolución de llamada para el cliente autorizado.
+ */
+function getNewToken(oAuth2Client, callback){
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES,
+    });
+    console.log('Autoriza esta app visitando esta url:', authUrl);
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    rl.question('Ingresa el código obtenido de la página aquí: ', (code)=>{
+        rl.close();
+        oAuth2Client.getToken(code, (err, token)=>{
+            if(err) return console.error('Error obteniendo el token de acceso', err);
+            oAuth2Client.setCredentials(token);
+            //Almacenar el token en el disco para las próximas ejecuciones de los programas
+            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err)=>{
+                if(err) return console.error(err);
+                console.log('Token almacenado en', TOKEN_PATH);
+            });
+            callback(oAuth2Client);
+        });
+    });
+}
+
+/**
+ * Listar las etiquetas obtenidas en la cuenta del usuario.
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function listLabels(auth){
+    const gmail = google.gmail({version: 'v1', auth});
+    gmail.users.labels.list({
+        userId: 'me',
+    }, (err, res)=>{
+        if(err) return console.log('La API ha retornado un error: '+err);
+        const labels = res.data.labels;
+        if(labels.length){
+            console.log('Etiquetas (categorías):');
+            labels.forEach((label)=>{
+                console.log(`-> ${label.name}`);
+            });
+        } else {
+            console.log('No se encontraron etiquetas')
+        }
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
